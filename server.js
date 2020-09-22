77ee6458dc28b1299867b6c1dc20e2e442e6a0f3
SergeyArtemyev <sorg.true@gmail.com>
2020-09-22 13:22:01 +0300

    set up api routes

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 // Connect Databse
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
